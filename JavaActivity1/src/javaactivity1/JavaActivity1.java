d108693b66ce31a7110ad0e04a2a5626cee39ab0
JpoyCachero <57879906+JpoyCachero@users.noreply.github.com>
2019-11-25 09:50:35 +0800

    Add files via upload

---
+++ JavaActivity1/src/javaactivity1/JavaActivity1.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaactivity1;
+
+/**
+ *
+ * @author it11621
+ */
+public class JavaActivity1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         // TODO code application logic here
+        int q=5;
+        for (int i = q; i >= 1; i--)
+        {
+            for (int j = i; j >= 1; j--)
+            {
+                System.out.print(  " *");
+            }
+            System.out.println();
+        }
+
+        for (int i = 2; i <= q; i++)
+        {
+            for (int j = i; j >= 1; j--)
+            {
+                System.out.print( " *");
+            }
+            System.out.println();
+        }
+    }
+}
+
+        // TODO code application logic here
+   
